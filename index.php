32c88a39c8fa16df06ef6b8eeae65f9f25ef3d6f
Simon Moore <simon@fotosim.co.uk>
2018-08-13 15:51:47 +0100

    Redesigned image gallery showcase.

--- index.php
+++ index.php
@@ -4,9 +4,17 @@
     // echo date('Y'); // Year
     // echo date('l'); // Day of week
 
-    echo 'Today\'s date is '.date('l').' '.date('d').'/'.date('m').'/'.date('y').'.<br>';
+    // echo 'Today\'s date is '.date('l').' '.date('d').'/'.date('m').'/'.date('y').'.<br>';
 
-    echo date('h:i:sa').'<br>';
+    // echo date('h:i:sa').'<br>';
 
-    echo date('d/m/y - h:i:sa', mktime(16,24,0,4,3,1972)).'<br>';
+    // echo date('d/m/y - h:i:sa', mktime(16,24,0,4,3,1972)).'<br>';
+
+    // echo $_SERVER['SERVER_SOFTWARE'].'<BR>';
+    // echo date('d/m/y - h:i:sa', $_SERVER['REQUEST_TIME']).'<BR>';
+    // echo $_SERVER['SERVER_ADMIN'].'<BR>';
+
+    foreach ($_SERVER as $key => $value) {
+        echo $key.' : '.$value.'<BR>';
+    }
 ?>
